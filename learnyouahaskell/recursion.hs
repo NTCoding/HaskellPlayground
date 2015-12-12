5b87b27c2c357743c598ca694d5256e7fe0ca020
nick <nick.tune@artirix.com>
2015-12-12 18:16:55 +0000

    quick sort

--- learnyouahaskell/recursion.hs
+++ learnyouahaskell/recursion.hs
@@ -8,3 +8,10 @@ take' n _
   | n <= 0 = []
 take' _ [] = []
 take' n (x:xs) =  x : take'(n - 1) xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =  
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in smallerSorted ++ [x] ++ biggerSorted
