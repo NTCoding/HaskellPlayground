afa47e16cffe5bbb2281af814673f62288dab2a3
nick <nick.tune@artirix.com>
2015-12-13 19:27:22 +0000

    more hof examples

--- learnyouahaskell/higherorderfunctions.hs
+++ learnyouahaskell/higherorderfunctions.hs
@@ -15,4 +15,24 @@ zipFilter _ _ [] = []
 zipFilter f (a:as) (b:bs) = 
   case f (a,b) of True  -> (a,b) : zipFilter f as bs
                   False -> zipFilter f as bs
+
+
+funcDestruct :: (String -> String -> c) -> (String -> String -> String)
+funcDestruct f x y = "We got " ++ x ++ y
+
+funco :: String -> String -> String
+funco = \x y -> "YOLO " ++ x ++ y
+
+sumSquareOddsBelow10k =
+  let odds = [1,3..9999]
+      squared = [n * n | n <- odds]
+  in  sum squared
+
+collatz :: Int -> [Int]
+collatz 1 = [1]
+collatz n =
+  case n `mod` 2 of 0 -> n : collatz (n `div` 2)
+                    1 -> n : collatz ((n * 3) + 1)
+
+
   
